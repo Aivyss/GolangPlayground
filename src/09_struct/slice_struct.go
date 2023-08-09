69b44837bccac0e0d14655008b0f568e3d102e95
Aivyss <hklee6417@gmail.com>
2023-08-10 04:02:26 +0900

    ADD slice struct

--- src/09_struct/slice_struct.go
+++ src/09_struct/slice_struct.go
@@ -5,6 +5,11 @@ import "fmt"
 /*
 slice structure
 */
+type item struct {
+	name  string
+	price int
+}
+type betterCart []item
 type cart []struct {
 	name  string
 	price int
@@ -23,4 +28,18 @@ func main() {
 	}
 
 	fmt.Println(crt)
+	fmt.Printf("%T\n", crt[0])
+
+	bc := betterCart{
+		{
+			name:  "itemA",
+			price: 1000,
+		},
+		{
+			name:  "itemB",
+			price: 2000,
+		},
+	}
+	fmt.Println(bc)
+	fmt.Printf("%T\n", bc[0])
 }
